59c5f81b94c1123f1ce1a3719327e4452b472f36
Adrian <murilloadrianc@gmail.com>
2022-04-02 21:59:17 -0300

    Add background animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import { Layout } from "../layout/Layout";
 import Hero from "../components/Hero/Hero";
-console.log(Layout);
+import { Section } from "../styles/GlobalComponents";
+import BgAnimation from "../components/BackgroundAnimation/BgAnimation";
+
 function Home() {
   return (
     <Layout>
+      <Section grid />
       <Hero />
+      <BgAnimation />
+      <Section />
     </Layout>
   );
 }
