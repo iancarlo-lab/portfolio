c14085a64ce16c76cf21e08c0a9347f754e28fa2
Adrian <murilloadrianc@gmail.com>
2022-04-02 21:35:48 -0300

    Add Hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,26 @@
-function Hero() {
-  return <div>Hero</div>;
-}
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyle";
+
+const Hero = () => (
+  <Section>
+    <LeftSection>
+      <SectionTitle main center>
+        Adrian Murillo <br />
+        Mechatronics Engineer | Full Stack Developer | Robotics
+      </SectionTitle>
+      <SectionText>
+        +10 years experience in Tech companies | OEM | IT | Robotics
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
 
 export default Hero;
