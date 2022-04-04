a73c0ffb0b1aa0b4305d56167ec6f8d3be2a9106
Adrian <murilloadrianc@gmail.com>
2022-04-03 21:56:43 -0300

    Add Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,48 @@
-function Footer() {
-  return <div>Footer</div>;
-}
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+
+import { SocialIcons } from "../Header/HeaderStyle";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyle";
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:5 5 5 5 5">5 5 5 55</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:contact@adrian.com">my email.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>My slogan yeah right</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://google.com" target="_blank">
+          <AiFillGithub />
+        </SocialIcons>
+        <SocialIcons href="https://google.com" target="_blank">
+          <AiFillLinkedin />
+        </SocialIcons>
+        <SocialIcons href="https://google.com" target="_blank">
+          <AiFillInstagram />
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
 
 export default Footer;
